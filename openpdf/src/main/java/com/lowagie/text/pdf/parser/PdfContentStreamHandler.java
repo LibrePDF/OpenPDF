8766d17d52480e61e6eba8a1d89a5837fe38c793
Alberto Fernandez <infjaf@gmail.com>
2018-01-14 20:48:26 +0100

    remove unused imports

--- openpdf/src/main/java/com/lowagie/text/pdf/parser/PdfContentStreamHandler.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/parser/PdfContentStreamHandler.java
@@ -47,7 +47,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Stack;
