12aac9c25e19580ab3f16b8927cc4657af8767d9
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2018-08-29 10:58:08 +0200

    Add unit test to stabilize visible signature

---
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfSignatureAppearanceTest.java
@@ -0,0 +1,134 @@
+package com.lowagie.text.pdf;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Rectangle;
+
+public class PdfSignatureAppearanceTest {
+
+	@Test
+	public void invisibleExternalSignature() throws DocumentException, IOException, NoSuchAlgorithmException {
+		byte[] expectedDigestPreClose = null;
+		byte[] expectedDigestClose = null;
+
+		// These fields are provided to be able to generate the same content more than
+		// once
+		Calendar signDate = Calendar.getInstance();
+		PdfObject overrideFileId = new PdfLiteral("<123><123>".getBytes());
+
+		for (int i = 0; i < 10; i++) {
+			try (InputStream is = getClass().getResourceAsStream("/EmptyPage.pdf"); ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+				PdfReader reader = new PdfReader(is);
+				PdfStamper stp = PdfStamper.createSignature(reader, baos, '\0', null, true);
+				stp.setEnforcedModificationDate(signDate);
+				stp.setOverrideFileId(overrideFileId);
+
+				PdfSignatureAppearance sap = stp.getSignatureAppearance();
+
+				PdfDictionary dic = new PdfDictionary();
+				dic.put(PdfName.FILTER, PdfName.ADOBE_PPKLITE);
+				dic.put(PdfName.M, new PdfDate(signDate));
+
+				sap.setCryptoDictionary(dic);
+				sap.setSignDate(signDate);
+				sap.setCertificationLevel(2);
+				sap.setReason("Test");
+
+				HashMap exc = new HashMap();
+				exc.put(PdfName.CONTENTS, new Integer(10));
+				sap.preClose(exc);
+
+				byte[] result = IOUtils.toByteArray(sap.getRangeStream());
+				byte[] sha256 = getSHA256(result);
+				if (expectedDigestPreClose == null) {
+					expectedDigestPreClose = sha256;
+				} else {
+					assertArrayEquals(expectedDigestPreClose, sha256);
+				}
+
+				PdfDictionary update = new PdfDictionary();
+				update.put(PdfName.CONTENTS, new PdfString("aaaa").setHexWriting(true));
+				sap.close(update);
+
+				byte[] resultClose = IOUtils.toByteArray(sap.getRangeStream());
+				byte[] sha256Close = getSHA256(resultClose);
+				if (expectedDigestClose == null) {
+					expectedDigestClose = sha256Close;
+				} else {
+					assertArrayEquals(expectedDigestClose, sha256Close);
+				}
+			}
+		}
+	}
+
+	@Test
+	public void visibleExternalSignature() throws DocumentException, IOException, NoSuchAlgorithmException {
+		byte[] expectedDigestPreClose = null;
+		byte[] expectedDigestClose = null;
+
+		Calendar signDate = Calendar.getInstance();
+		PdfObject overrideFileId = new PdfLiteral("<123><123>".getBytes());
+
+		for (int i = 0; i < 10; i++) {
+			try (InputStream is = getClass().getResourceAsStream("/EmptyPage.pdf"); ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+				PdfReader reader = new PdfReader(is);
+				PdfStamper stp = PdfStamper.createSignature(reader, baos, '\0', null, true);
+				stp.setEnforcedModificationDate(signDate);
+				stp.setOverrideFileId(overrideFileId);
+
+				PdfSignatureAppearance sap = stp.getSignatureAppearance();
+
+				PdfDictionary dic = new PdfDictionary();
+				dic.put(PdfName.FILTER, PdfName.ADOBE_PPKLITE);
+				dic.put(PdfName.M, new PdfDate(signDate));
+
+				sap.setCryptoDictionary(dic);
+				sap.setSignDate(signDate);
+				sap.setVisibleSignature(new Rectangle(100, 100), 1);
+				sap.setLayer2Text("Hello world");
+
+				HashMap exc = new HashMap();
+				exc.put(PdfName.CONTENTS, new Integer(10));
+				sap.preClose(exc);
+
+				byte[] result = IOUtils.toByteArray(sap.getRangeStream());
+				byte[] sha256 = getSHA256(result);
+				if (expectedDigestPreClose == null) {
+					expectedDigestPreClose = sha256;
+				} else {
+					assertArrayEquals(expectedDigestPreClose, sha256);
+				}
+
+				PdfDictionary update = new PdfDictionary();
+				update.put(PdfName.CONTENTS, new PdfString("aaaa").setHexWriting(true));
+				sap.close(update);
+
+				byte[] resultClose = IOUtils.toByteArray(sap.getRangeStream());
+				byte[] sha256Close = getSHA256(resultClose);
+				if (expectedDigestClose == null) {
+					expectedDigestClose = sha256Close;
+				} else {
+					assertArrayEquals(expectedDigestClose, sha256Close);
+				}
+			}
+		}
+	}
+
+	private byte[] getSHA256(byte[] bytes) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		return md.digest(bytes);
+	}
+
+}
