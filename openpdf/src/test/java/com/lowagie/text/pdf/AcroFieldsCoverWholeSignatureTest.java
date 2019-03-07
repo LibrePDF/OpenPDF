0a265f6b5693dfb8910a4f717fa7c338d16acc31
Beat Weisskopf <weisskopf@fenceit.ch>
2019-03-07 18:42:32 +0100

    clean commit for issue #158 (signature wrapping attack)

---
+++ openpdf/src/test/java/com/lowagie/text/pdf/AcroFieldsCoverWholeSignatureTest.java
@@ -0,0 +1,43 @@
+package com.lowagie.text.pdf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.security.Security;
+import java.util.ArrayList;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.PageSize;
+
+public class AcroFieldsCoverWholeSignatureTest {
+
+    /**
+     * This test fails, because signatureCoversWholeDocument does only check the
+     * last signed block.
+     */
+    @Test
+    public void testGetSignatures() throws Exception {
+        // for algorithm SHA256 (without dash)
+        Security.addProvider(new BouncyCastleProvider());
+        InputStream moddedFile = AcroFieldsCoverWholeSignatureTest.class.getResourceAsStream("/siwa.pdf");
+        PdfReader reader = new PdfReader(moddedFile);
+        Document document = new Document(PageSize.A4);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PdfWriter writer = PdfWriter.getInstance(document, out);
+
+        AcroFields fields = new AcroFields(reader, writer);
+        ArrayList<String> names = fields.getSignatureNames();
+        Assertions.assertEquals(1, names.size());
+
+        for (String signName : names) {
+            Assertions.assertFalse(fields.signatureCoversWholeDocument(signName));
+            PdfPKCS7 pdfPkcs7 = fields.verifySignature(signName, "BC");
+            Assertions.assertTrue(pdfPkcs7.verify());
+        }
+
+    }
+
+}
