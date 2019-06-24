6db044f45718d08ba24eb6ff7e93812eb63c3338
Andreas Røsdal <andreas.rosdal@gmail.com>
2019-06-24 08:26:02 +0200

    Cleanup.

--- openpdf/src/test/java/com/lowagie/text/pdf/sign/ExtractCertificatesTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/sign/ExtractCertificatesTest.java
@@ -15,62 +15,58 @@ import com.lowagie.text.pdf.PdfReader;
 
 public class ExtractCertificatesTest {
 
-	public ExtractCertificatesTest() {
-		super();
-	}
+    public ExtractCertificatesTest() {
+        super();
+    }
 
 
-	@Test
-	public void testSha1() throws Exception {
-		extract("src/test/resources/sample_signed-sha1.pdf");
-	}
+    @Test
+    public void testSha1() throws Exception {
+        extract("src/test/resources/sample_signed-sha1.pdf");
+    }
 
-	@Test
-	public void testSha512() throws Exception {
-		extract("src/test/resources/sample_signed-sha512.pdf");
-	}
+    @Test
+    public void testSha512() throws Exception {
+        extract("src/test/resources/sample_signed-sha512.pdf");
+    }
 
-	private void extract(String pdf) throws Exception {
-		
-		// Based in : https://github.com/iromu/pdf-signature-itext/blob/master/src/main/java/crypto/PDFSignature.java
-		
-		List<X509Certificate> certificates = new ArrayList<X509Certificate>();
-		System.out.println("pdf name: " + pdf);
+    private void extract(String pdf) throws Exception {
 
-		KeyStore kall = PdfPKCS7.loadCacertsKeyStore();
+        List<X509Certificate> certificates = new ArrayList<X509Certificate>();
+        System.out.println("pdf name: " + pdf);
 
-		// Cargar pdf
-		try (PdfReader reader = new PdfReader(pdf)) {
-			AcroFields fields = reader.getAcroFields();
-	
-			ArrayList<String> signatures = fields.getSignatureNames();
-			System.out.println("Signs: " + signatures.size());
-			for (String signature : signatures) {
-	
-				System.out.println("Signature name: " + signature);
-				System.out.println("Signature covers whole document: " + fields.signatureCoversWholeDocument(signature));
-				System.out.println(
-						"Document revision: " + fields.getRevision(signature) + " of " + fields.getTotalRevisions());
-	
-				PdfPKCS7 pk = fields.verifySignature(signature);
-				Calendar cal = pk.getSignDate();
-				Certificate[] pkc = pk.getCertificates();
-				X509Certificate certificate = pk.getSigningCertificate();
-				certificates.add(certificate);
-				System.out.println("sign date:" + cal.getTime());
-				System.out.println("Subject: " + PdfPKCS7.getSubjectFields(certificate));
-				System.out.println("Document modified: " + !pk.verify());
+        KeyStore kall = PdfPKCS7.loadCacertsKeyStore();
 
-				Object[] fails = PdfPKCS7.verifyCertificates(pkc, kall, null, cal);
-				if (fails == null) {
-					System.out.println("Certificates verified against the KeyStore");
-				}
-				else {
-					System.out.println("Certificate failed: " + fails[1]);
-				}				
-	
-			}
-		}
+        try (PdfReader reader = new PdfReader(pdf)) {
+            AcroFields fields = reader.getAcroFields();
 
-	}
+            ArrayList<String> signatures = fields.getSignatureNames();
+            System.out.println("Signs: " + signatures.size());
+            for (String signature : signatures) {
+
+                System.out.println("Signature name: " + signature);
+                System.out.println("Signature covers whole document: " + fields.signatureCoversWholeDocument(signature));
+                System.out.println(
+                        "Document revision: " + fields.getRevision(signature) + " of " + fields.getTotalRevisions());
+
+                PdfPKCS7 pk = fields.verifySignature(signature);
+                Calendar cal = pk.getSignDate();
+                Certificate[] pkc = pk.getCertificates();
+                X509Certificate certificate = pk.getSigningCertificate();
+                certificates.add(certificate);
+                System.out.println("sign date:" + cal.getTime());
+                System.out.println("Subject: " + PdfPKCS7.getSubjectFields(certificate));
+                System.out.println("Document modified: " + !pk.verify());
+
+                Object[] fails = PdfPKCS7.verifyCertificates(pkc, kall, null, cal);
+                if (fails == null) {
+                    System.out.println("Certificates verified against the KeyStore");
+                } else {
+                    System.out.println("Certificate failed: " + fails[1]);
+                }
+
+            }
+        }
+
+    }
 }
