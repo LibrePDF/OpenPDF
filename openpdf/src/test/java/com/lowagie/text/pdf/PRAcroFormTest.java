266740990fbf49f200732095212ecbcd3359a304
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2019-11-20 13:20:41 +0100

    name input class fix

--- openpdf/src/test/java/com/lowagie/text/pdf/PRAcroFormTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/PRAcroFormTest.java
@@ -11,7 +11,7 @@ public class PRAcroFormTest {
 	
     @Test
     public void infiniteLoopTest() throws Exception {
-        try (InputStream is = AcroFieldsTest.class.getResourceAsStream("/pades_opposite_infinite_loop.pdf");
+        try (InputStream is = PRAcroFormTest.class.getResourceAsStream("/pades_opposite_infinite_loop.pdf");
                 PdfReader reader = new PdfReader(is)) {
             assertTimeoutPreemptively(ofMillis(500), () -> {
     			reader.getAcroForm();
