4d8bf6655ff0ee5bbc269dc4a0d1a6c994413b61
Gajendra kumar <raaz2.gajendra@gmail.com>
2020-01-20 18:02:36 +0530

    Update TTFCache.java

--- openpdf/src/main/java/com/lowagie/text/pdf/TTFCache.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/TTFCache.java
@@ -1,11 +1,16 @@
 package com.lowagie.text.pdf;
 
 import org.apache.fop.fonts.apps.TTFReader;
+import org.apache.fop.fonts.truetype.FontFileReader;
+import org.apache.fop.fonts.truetype.OFFontLoader;
 import org.apache.fop.fonts.truetype.TTFFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.lowagie.text.ExceptionConverter;
 
 /**
  *
@@ -23,14 +28,36 @@ public class TTFCache {
         TTFReader app = new TTFReader();
         TTFFile ttf =null;
         try {
-            ttf = app.loadTTF(fileName, null, true, true);
+            ttf = loadTTF(app,fileName);
             ttfFileMap.put(fileName,ttf);
             return ttf;
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new ExceptionConverter(e);
         }
-        return null;
     }
 
-
+    private static TTFFile loadTTF(TTFReader app, String fileName) throws IOException{
+        try {
+            return app.loadTTF(fileName, null, true, true);
+        } catch (IOException e) {
+            TTFFile ttfFile = new TTFFile(true, true);
+            InputStream stream = BaseFont.getResourceStream(fileName, null);
+            try {
+                FontFileReader reader = new FontFileReader(stream);
+                String header = OFFontLoader.readHeader(reader);
+                String fontName = null;
+                boolean supported = ttfFile.readFont(reader, header, fontName);
+                if (!supported) {
+                    return null;
+                }
+            } finally {
+                stream.close();
+            }
+            if (ttfFile.isCFF()) {
+                throw new UnsupportedOperationException(
+                        "OpenType fonts with CFF data are not supported, yet");
+            }
+            return ttfFile;
+        }
+    }
 }
