98134fe9b7d226fa7494030527f9fce769a482d4
Wugengxian <60342704+Wugengxian@users.noreply.github.com>
2021-04-17 01:03:11 +0800

    Update PdfCopyFieldsImp.java

--- openpdf/src/main/java/com/lowagie/text/pdf/PdfCopyFieldsImp.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/PdfCopyFieldsImp.java
@@ -217,7 +217,7 @@ class PdfCopyFieldsImp extends PdfWriter {
                 for (PdfObject ob : ((PdfArray)obj).getElements()) {
                     if (ob != null && ob.isIndirect()) {
                         PRIndirectReference ind = (PRIndirectReference) ob;
-                        if (!isVisited(ind) && !isPage(ind)) {
+                        if (!isVisited(ind) && !isPage(ind) && ((PRIndirectReference) ob).getReader().getPdfObject(ind.getNumber()) != null) {
                             PdfIndirectReference ref = getNewReference(ind);
                             propagate(PdfReader.getPdfObjectRelease(ind), restricted);
                         }
