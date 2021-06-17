9b9b2a83848b63b2083eea1f649a4ae3ac88f25c
mkl <klink@csi.com>
2021-06-17 18:28:05 +0200

    #375 - Implement AES256 (ISO 32000-2) support
    DecryptAES256R6Test now tests this POC also using the example file Demo1_encrypted_.pdf provided by TvT.
    
    #375 - "Unknown encryption type R = 6" support AES256
    https://github.com/LibrePDF/OpenPDF/issues/375
    Demo1_encrypted_.pdf provided by TvT

--- openpdf/src/test/java/com/lowagie/text/pdf/DecryptAES256R6Test.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/DecryptAES256R6Test.java
@@ -35,8 +35,29 @@ class DecryptAES256R6Test {
         try (   InputStream resource = getClass().getResourceAsStream("/issue375/pwProtectedAES256_openPDFiss375.pdf")  ) {
             PdfReader pdfReader = new PdfReader(resource);
             Assertions.assertTrue(pdfReader.isEncrypted(), "PdfReader fails to report test file to be encrypted.");
-            Assertions.assertEquals(1, pdfReader.getNumberOfPages(), "PdfReder fails to report the correct number of pages");
+            Assertions.assertEquals(1, pdfReader.getNumberOfPages(), "PdfReader fails to report the correct number of pages");
             Assertions.assertEquals("TEST", new PdfTextExtractor(pdfReader).getTextFromPage(1), "Wrong text extracted from page 1");
+            pdfReader.close();
+        }
+    }
+
+    /**
+     * <a href="https://github.com/LibrePDF/OpenPDF/issues/375">
+     * "Unknown encryption type R = 6" support AES256
+     * </a>
+     * <br>
+     * Demo1_encrypted_.pdf provided by TvT
+     * <p>
+     * This test method checks whether OpenPdf can correctly decrypt
+     * a file which is AES256 encrypted according to ISO 32000-2.
+     */
+    @Test
+    void testReadDemo1Encrypted() throws IOException {
+        try (   InputStream resource = getClass().getResourceAsStream("/issue375/Demo1_encrypted_.pdf")  ) {
+            PdfReader pdfReader = new PdfReader(resource);
+            Assertions.assertTrue(pdfReader.isEncrypted(), "PdfReader fails to report test file to be encrypted.");
+            Assertions.assertEquals(1, pdfReader.getNumberOfPages(), "PdfReader fails to report the correct number of pages");
+            Assertions.assertEquals("Demo   Name   Signature   Date  Elizabeth Schultz (Apr 24, 2018) Elizabeth Schultz Apr 24, 2018 Elizabeth Schultz Sue Northrop (Apr 24, 2018) Apr 24, 2018 Sue Northrop", new PdfTextExtractor(pdfReader).getTextFromPage(1), "Wrong text extracted from page 1");
             pdfReader.close();
         }
     }
