0bdadb61267a428e89edaec0588d449dc8484555
YourLord <jtang@fandm.edu>
2021-11-21 21:45:30 -0500

    issue613

---
+++ openpdf/src/test/java/com/lowagie/text/pdf/IssueTest613.java
@@ -0,0 +1,30 @@
+package com.lowagie.text.pdf;
+
+import com.lowagie.text.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IssueTest613 {
+
+    public static void main(String[] args) {
+
+        // Register the font which we want to supports Hindi
+        FontFactory.register("C:\\Windows\\Fonts\\NIRMALA.TTF");
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document,
+                    new FileOutputStream("D:\\workspace\\TMP\\out\\HelloWorld.pdf"));
+            document.open();
+            document.add(new Chunk(
+                    "नमस्ते",
+                    FontFactory.getFont("nirmalaui-bold", "Identity-H", false, 10F, 0, null)));
+        } catch (IOException ioexcep) {
+            System.err.println(ioexcep.getMessage());
+        } catch (DocumentException docexcep) {
+            System.err.println(docexcep.getMessage());
+        }finally {
+            document.close();
+        }
+    }
+}
