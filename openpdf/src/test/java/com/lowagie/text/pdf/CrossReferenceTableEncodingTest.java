dfcdb30bc28017b5c8f52362ed087f20efd491a7
Claudio Clemens <asturio@gmx.net>
2022-03-13 01:36:40 +0100

    Make CrossReferenceTableEncodingTest work in Java 16
    - removed hack using reflection for Charset.defaultEncoding

--- openpdf/src/test/java/com/lowagie/text/pdf/CrossReferenceTableEncodingTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/CrossReferenceTableEncodingTest.java
@@ -1,67 +1,41 @@
 package com.lowagie.text.pdf;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
-
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Test;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.Paragraph;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
 
-@SuppressWarnings("nls")
 public class CrossReferenceTableEncodingTest {
 
-    private static final String FILE_ENCODING = "file.encoding";
+    public static final String TEST_PDF = "/encodingTest.pdf";
 
+    // This test was once red, even it was not easy to accomplish this. The test must be run with
+    // -Dfile.encoding=IBM273 from the IDE. Maven won't accept this property.
     @Test
     public void testCrossReferenceTableEncoding() throws Exception {
-        final String oldEncoding = System.getProperty(FILE_ENCODING);
-        try {
-            // Change Encoding to IBM-273
-            System.setProperty(FILE_ENCODING, "IBM273");
-            resetDefaultCharset();
-
-            final String actualPDF = this.generateSimplePdf();
-            final String expectedPDF = this.readExpectedFile("/encodingTest.pdf");
-            assertEquals(filterPdf(expectedPDF), filterPdf(actualPDF));
-        } finally {
-
-            // Finally reset original charset
-            if (oldEncoding == null) {
-                System.clearProperty(FILE_ENCODING);
-            } else {
-                System.setProperty(FILE_ENCODING, oldEncoding);
-            }
-            resetDefaultCharset();
-        }
-    }
-
-    /**
-     * Resets the defaultCharset within Charset class, so that the Systemproperty file.encoding will be evaluated.
-     */
-    private static void resetDefaultCharset() throws NoSuchFieldException, IllegalAccessException {
-        final Field charset = Charset.class.getDeclaredField("defaultCharset");
-        charset.setAccessible(true);
-        charset.set(null, null);
+        final String actualPDF = generateSimplePdf();
+        final String expectedPDF = readExpectedFile();
+        String actual = filterPdf(actualPDF);
+        String expected = filterPdf(expectedPDF);
+        assertThat(actual).isEqualTo(expected);
     }
 
     private static String filterPdf(final String pdf) {
-        return pdf.replaceAll("<<\\/ModDate.*?>>", "<</ModDate XXXXX>>")
-                  .replaceAll("<</Info .*?>>", "<<\\/Info XXXXX>>")
-                  .replaceAll("startxref\\n(\\d+)\\n%%EOF", "startxref\\n(XXXXX)\\n%%EOF");
+        return pdf.replaceAll("<</ModDate.*?>>", "<</ModDate XXXXX>>")
+            .replaceAll("<</Info .*?>>", "<</Info XXXXX>>")
+            .replaceAll("startxref\\n(\\d+)\\n%%EOF", "startxref\nXXXXX\n%%EOF");
     }
 
-    private String readExpectedFile(final String resourceName) throws IOException {
-        try (final InputStream expected  = this.getClass().getResourceAsStream(resourceName)) {
-            assertNotNull(expected);
-            return IOUtils.toString(expected, "ISO-8859-1");
+    private String readExpectedFile() throws IOException {
+        try (final InputStream expected = getClass().getResourceAsStream(TEST_PDF)) {
+            assertThat(expected).isNotNull();
+            return IOUtils.toString(expected, ISO_8859_1);
         }
     }
 
@@ -72,8 +46,7 @@ public class CrossReferenceTableEncodingTest {
                 document.newPage();
                 document.add(new Paragraph("Hello World!"));
             }
-            final byte[] actualBytes = out.toByteArray();
-            return new String(actualBytes, "ISO-8859-1");
+            return out.toString("ISO-8859-1");
         }
     }
 }
