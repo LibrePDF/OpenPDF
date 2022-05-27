a3af5873d4bced3b7021d710a6f168fee8509994
mm <dymzh11036@126.com>
2022-05-27 11:40:03 +0800

    Update MayNotBreakTest.java

--- openpdf/src/test/java/com/lowagie/text/pdf/MayNotBreakTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/MayNotBreakTest.java
@@ -2,14 +2,39 @@ package com.lowagie.text.pdf;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class MayNotBreakTest {
+/**
+ * @author  SE_SUSTech, group: Lanrand
+ * test issue #659
+ * <p>This file is to test the code for add the enhancement in the issue #659
+ * and there are 2 test cases
+ */
+public class MayNotBreakTest {//NOPMD
+
+    /**
+     * It is test the feature of mayNotBreak
+     * It will test when we set it True
+     */
     @Test
-    void shouldReturnWhetherBreak() throws Exception {
-        PdfPCell[] cells = new PdfPCell[3];
-        PdfPRow row = new PdfPRow(cells);
+    public static void shouldReturnWhetherBreak1(){
+        final PdfPCell[] cells = new PdfPCell[2];
+        final PdfPRow row = new PdfPRow(cells);
         row.setMayNotBreak(true);
-        assertTrue(row.isMayNotBreak());
+        assertTrue(row.isMayNotBreak(),"It's cannot break");
     }
+
+    /**
+     * It is test the feature of mayNotBreak
+     * It will test when we set it False
+     */
+    @Test
+    public static void shouldReturnWhetherBreak2() {
+        final PdfPCell[] cells = new PdfPCell[3];
+        final PdfPRow row = new PdfPRow(cells);
+        row.setMayNotBreak(false);
+        assertFalse(row.isMayNotBreak(),"It's can break");
+    }
+
+
 }
