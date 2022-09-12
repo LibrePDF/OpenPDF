1feeeac294ecc9e086ad1e4e74971cb99eb41d89
Claudio Clemens <asturio@gmx.net>
2022-09-12 18:36:40 +0200

    Checkstyle fixes

--- openpdf/src/test/java/com/lowagie/text/pdf/table/TableEndlessTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/table/TableEndlessTest.java
@@ -1,14 +1,18 @@
 package com.lowagie.text.pdf.table;
 
-import com.lowagie.text.*;
+import static java.time.Duration.ofSeconds;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import com.lowagie.text.Cell;
+import com.lowagie.text.Document;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Table;
 import com.lowagie.text.alignment.VerticalAlignment;
 import com.lowagie.text.pdf.PdfWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
-
-import java.io.FileOutputStream;
-
-import static java.time.Duration.*;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 public class TableEndlessTest {
 
@@ -19,7 +23,7 @@ public class TableEndlessTest {
     void testNoEndlessLoopWithBottom() {
         assertTimeout(ofSeconds(10), () -> {
             Document document = new Document(PageSize.A4);
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("test.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(document, Files.newOutputStream(Paths.get("test.pdf")));
             Table table = new Table(2);
             Cell cell1 = new Cell("any text");
             cell1.setVerticalAlignment(VerticalAlignment.BOTTOM);
@@ -43,7 +47,7 @@ public class TableEndlessTest {
     void testNoEndlessLoopWithCenter() {
         assertTimeout(ofSeconds(10), () -> {
             Document document = new Document(PageSize.A4);
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("test.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(document, Files.newOutputStream(Paths.get("test.pdf")));
             Table table = new Table(2);
             Cell cell1 = new Cell("any text\nany text\nany text");
             cell1.setVerticalAlignment(VerticalAlignment.CENTER);
