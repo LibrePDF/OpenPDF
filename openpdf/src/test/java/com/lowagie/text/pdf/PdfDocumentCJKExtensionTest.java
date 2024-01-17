d3d4d4b8e89f0940b1087a60757427044564405e
Andreas Røsdal <andreas.rosdal@gmail.com>
2024-01-17 22:57:30 +0100

    Fix unused imports.

--- openpdf/src/test/java/com/lowagie/text/pdf/PdfDocumentCJKExtensionTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfDocumentCJKExtensionTest.java
@@ -3,8 +3,6 @@ package com.lowagie.text.pdf;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.junit.jupiter.api.Test;
-
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.Font;
