c90d8bfac13dc5c079b2cac70af9f47fdaff481f
Claudio Clemens <asturio@gmx.net>
2024-02-20 20:54:12 +0100

    Add more coverage
    - Codacy Review
    
    (cherry picked from commit 57492291767df963fc2da131cb01c3d6f2cd735e)

--- openpdf/src/test/java/com/lowagie/text/DocumentTest.java
+++ openpdf/src/test/java/com/lowagie/text/DocumentTest.java
@@ -13,8 +13,37 @@ class DocumentTest {
 
         // Then
         assertThat(versionInCode)
-                .as("Version number in code %s is not correct.", versionInCode)
-                .matches("^OpenPDF \\d+\\.\\d+\\.\\d+(-SNAPSHOT)?$");
+            .as("Version number in code %s is not correct.", versionInCode)
+            .matches("^OpenPDF \\d+\\.\\d+\\.\\d+(-SNAPSHOT)?$");
+    }
+
+    @Test
+    void whenSetFootsToValueShouldSetObject() {
+        // Given
+        try (Document document = new Document()) {
+            HeaderFooter footer = new HeaderFooter(new Phrase("Footer"), false);
+
+            // When
+            document.setFooter(footer);
+
+            // Then
+            assertThat(document.footer).isEqualTo(footer);
+        }
+    }
+
+    @Test
+    void whenResetFooterShouldSetNull() {
+        // Given
+        try (Document document = new Document()) {
+            HeaderFooter footer = new HeaderFooter(new Phrase("Footer"), false);
+            document.setFooter(footer);
+
+            // When
+            document.resetFooter();
+
+            // Then
+            assertThat(document.footer).isNull();
+        }
     }
 
 }
