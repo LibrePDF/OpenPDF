2e5a43744f18b7c1e4f1dfe8c88cc1ed8b5b7dcc
Radek Wikturna <radek.wikturna@usu.com>
2024-02-21 12:53:17 +0100

    #1059 font size specified in % is interpreted as pixels

--- openpdf/src/main/java/com/lowagie/text/html/Markup.java
+++ openpdf/src/main/java/com/lowagie/text/html/Markup.java
@@ -44,7 +44,7 @@
  *
  * Contributions by:
  * Lubos Strapko
- * 
+ *
  * If you didn't download this code from the following link, you should check if
  * you aren't using an obsolete version:
  * https://github.com/LibrePDF/OpenPDF
@@ -65,260 +65,386 @@ public class Markup {
 
     // iText specific
 
-    /** the key for any tag */
+    /**
+     * the key for any tag
+     */
     public static final String ITEXT_TAG = "tag";
 
     // HTML tags
 
-    /** the markup for the body part of a file */
+    /**
+     * the markup for the body part of a file
+     */
     public static final String HTML_TAG_BODY = "body";
 
-    /** The DIV tag. */
+    /**
+     * The DIV tag.
+     */
     public static final String HTML_TAG_DIV = "div";
 
-    /** This is a possible HTML-tag. */
+    /**
+     * This is a possible HTML-tag.
+     */
     public static final String HTML_TAG_LINK = "link";
 
-    /** The SPAN tag. */
+    /**
+     * The SPAN tag.
+     */
     public static final String HTML_TAG_SPAN = "span";
 
     // HTML attributes
 
-    /** the height attribute. */
+    /**
+     * the height attribute.
+     */
     public static final String HTML_ATTR_HEIGHT = "height";
 
-    /** the hyperlink reference attribute. */
+    /**
+     * the hyperlink reference attribute.
+     */
     public static final String HTML_ATTR_HREF = "href";
 
-    /** This is a possible HTML attribute for the LINK tag. */
+    /**
+     * This is a possible HTML attribute for the LINK tag.
+     */
     public static final String HTML_ATTR_REL = "rel";
 
-    /** This is used for inline css style information */
+    /**
+     * This is used for inline css style information
+     */
     public static final String HTML_ATTR_STYLE = "style";
 
-    /** This is a possible HTML attribute for the LINK tag. */
+    /**
+     * This is a possible HTML attribute for the LINK tag.
+     */
     public static final String HTML_ATTR_TYPE = "type";
 
-    /** This is a possible HTML attribute. */
+    /**
+     * This is a possible HTML attribute.
+     */
     public static final String HTML_ATTR_STYLESHEET = "stylesheet";
 
-    /** the width attribute. */
+    /**
+     * the width attribute.
+     */
     public static final String HTML_ATTR_WIDTH = "width";
 
-    /** attribute for specifying externally defined CSS class */
+    /**
+     * attribute for specifying externally defined CSS class
+     */
     public static final String HTML_ATTR_CSS_CLASS = "class";
 
-    /** The ID attribute. */
+    /**
+     * The ID attribute.
+     */
     public static final String HTML_ATTR_CSS_ID = "id";
 
     // HTML values
 
-    /** This is a possible value for the language attribute (SCRIPT tag). */
+    /**
+     * This is a possible value for the language attribute (SCRIPT tag).
+     */
     public static final String HTML_VALUE_JAVASCRIPT = "text/javascript";
 
-    /** This is a possible HTML attribute for the LINK tag. */
+    /**
+     * This is a possible HTML attribute for the LINK tag.
+     */
     public static final String HTML_VALUE_CSS = "text/css";
 
     // CSS keys
 
-    /** the CSS tag for background color */
+    /**
+     * the CSS tag for background color
+     */
     public static final String CSS_KEY_BGCOLOR = "background-color";
 
-    /** the CSS tag for text color */
+    /**
+     * the CSS tag for text color
+     */
     public static final String CSS_KEY_COLOR = "color";
 
-    /** CSS key that indicate the way something has to be displayed */
+    /**
+     * CSS key that indicate the way something has to be displayed
+     */
     public static final String CSS_KEY_DISPLAY = "display";
 
-    /** the CSS tag for the font family */
+    /**
+     * the CSS tag for the font family
+     */
     public static final String CSS_KEY_FONTFAMILY = "font-family";
 
-    /** the CSS tag for the font size */
+    /**
+     * the CSS tag for the font size
+     */
     public static final String CSS_KEY_FONTSIZE = "font-size";
 
-    /** the CSS tag for the font style */
+    /**
+     * the CSS tag for the font style
+     */
     public static final String CSS_KEY_FONTSTYLE = "font-style";
 
-    /** the CSS tag for the font weight */
+    /**
+     * the CSS tag for the font weight
+     */
     public static final String CSS_KEY_FONTWEIGHT = "font-weight";
 
-    /** the CSS tag for text decorations */
+    /**
+     * the CSS tag for text decorations
+     */
     public static final String CSS_KEY_LINEHEIGHT = "line-height";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_MARGIN = "margin";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_MARGINLEFT = "margin-left";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_MARGINRIGHT = "margin-right";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_MARGINTOP = "margin-top";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_MARGINBOTTOM = "margin-bottom";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_PADDING = "padding";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_PADDINGLEFT = "padding-left";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_PADDINGRIGHT = "padding-right";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_PADDINGTOP = "padding-top";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_PADDINGBOTTOM = "padding-bottom";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_BORDERCOLOR = "border-color";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_BORDERWIDTH = "border-width";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_BORDERWIDTHLEFT = "border-left-width";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_BORDERWIDTHRIGHT = "border-right-width";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_BORDERWIDTHTOP = "border-top-width";
 
-    /** the CSS tag for the margin of an object */
+    /**
+     * the CSS tag for the margin of an object
+     */
     public static final String CSS_KEY_BORDERWIDTHBOTTOM = "border-bottom-width";
 
-    /** the CSS tag for adding a page break when the document is printed */
+    /**
+     * the CSS tag for adding a page break when the document is printed
+     */
     public static final String CSS_KEY_PAGE_BREAK_AFTER = "page-break-after";
 
-    /** the CSS tag for adding a page break when the document is printed */
+    /**
+     * the CSS tag for adding a page break when the document is printed
+     */
     public static final String CSS_KEY_PAGE_BREAK_BEFORE = "page-break-before";
 
-    /** the CSS tag for the horizontal alignment of an object */
+    /**
+     * the CSS tag for the horizontal alignment of an object
+     */
     public static final String CSS_KEY_TEXTALIGN = "text-align";
 
-    /** the CSS tag for text decorations */
+    /**
+     * the CSS tag for text decorations
+     */
     public static final String CSS_KEY_TEXTDECORATION = "text-decoration";
 
-    /** the CSS tag for text decorations */
+    /**
+     * the CSS tag for text decorations
+     */
     public static final String CSS_KEY_VERTICALALIGN = "vertical-align";
 
-    /** the CSS tag for the visibility of objects */
+    /**
+     * the CSS tag for the visibility of objects
+     */
     public static final String CSS_KEY_VISIBILITY = "visibility";
 
     // CSS values
 
     /**
-     * value for the CSS tag for adding a page break when the document is
-     * printed
+     * value for the CSS tag for adding a page break when the document is printed
      */
     public static final String CSS_VALUE_ALWAYS = "always";
 
-    /** A possible value for the DISPLAY key */
+    /**
+     * A possible value for the DISPLAY key
+     */
     public static final String CSS_VALUE_BLOCK = "block";
 
-    /** a CSS value for text font weight */
+    /**
+     * a CSS value for text font weight
+     */
     public static final String CSS_VALUE_BOLD = "bold";
 
-    /** the value if you want to hide objects. */
+    /**
+     * the value if you want to hide objects.
+     */
     public static final String CSS_VALUE_HIDDEN = "hidden";
 
-    /** A possible value for the DISPLAY key */
+    /**
+     * A possible value for the DISPLAY key
+     */
     public static final String CSS_VALUE_INLINE = "inline";
 
-    /** a CSS value for text font style */
+    /**
+     * a CSS value for text font style
+     */
     public static final String CSS_VALUE_ITALIC = "italic";
 
-    /** a CSS value for text decoration */
+    /**
+     * a CSS value for text decoration
+     */
     public static final String CSS_VALUE_LINETHROUGH = "line-through";
 
-    /** A possible value for the DISPLAY key */
+    /**
+     * A possible value for the DISPLAY key
+     */
     public static final String CSS_VALUE_LISTITEM = "list-item";
 
-    /** a CSS value */
+    /**
+     * a CSS value
+     */
     public static final String CSS_VALUE_NONE = "none";
 
-    /** a CSS value */
+    /**
+     * a CSS value
+     */
     public static final String CSS_VALUE_NORMAL = "normal";
 
-    /** a CSS value for text font style */
+    /**
+     * a CSS value for text font style
+     */
     public static final String CSS_VALUE_OBLIQUE = "oblique";
 
-    /** A possible value for the DISPLAY key */
+    /**
+     * A possible value for the DISPLAY key
+     */
     public static final String CSS_VALUE_TABLE = "table";
 
-    /** A possible value for the DISPLAY key */
+    /**
+     * A possible value for the DISPLAY key
+     */
     public static final String CSS_VALUE_TABLEROW = "table-row";
 
-    /** A possible value for the DISPLAY key */
+    /**
+     * A possible value for the DISPLAY key
+     */
     public static final String CSS_VALUE_TABLECELL = "table-cell";
 
-    /** the CSS value for a horizontal alignment of an object */
+    /**
+     * the CSS value for a horizontal alignment of an object
+     */
     public static final String CSS_VALUE_TEXTALIGNLEFT = "left";
 
-    /** the CSS value for a horizontal alignment of an object */
+    /**
+     * the CSS value for a horizontal alignment of an object
+     */
     public static final String CSS_VALUE_TEXTALIGNRIGHT = "right";
 
-    /** the CSS value for a horizontal alignment of an object */
+    /**
+     * the CSS value for a horizontal alignment of an object
+     */
     public static final String CSS_VALUE_TEXTALIGNCENTER = "center";
 
-    /** the CSS value for a horizontal alignment of an object */
+    /**
+     * the CSS value for a horizontal alignment of an object
+     */
     public static final String CSS_VALUE_TEXTALIGNJUSTIFY = "justify";
 
-    /** a CSS value for text decoration */
+    /**
+     * a CSS value for text decoration
+     */
     public static final String CSS_VALUE_UNDERLINE = "underline";
 
-    /** a default value for font-size 
+    /**
+     * a default value for font-size
+     *
      * @since 2.1.3
      */
     public static final float DEFAULT_FONT_SIZE = 12f;
 
     /**
      * Parses a length.
-     * 
-     * @param string
-     *            a length in the form of an optional + or -, followed by a
-     *            number and a unit.
+     *
+     * @param string a length in the form of an optional + or -, followed by a number and a unit.
      * @return a float
      */
 
     public static float parseLength(String string) {
-        // TODO: Evaluate the effect of this.
-        // It may change the default behavour of the methd if this is changed.
-        // return parseLength(string, Markup.DEFAULT_FONT_SIZE);
         int pos = 0;
         int length = string.length();
         boolean ok = true;
         while (ok && pos < length) {
             switch (string.charAt(pos)) {
-            case '+':
-            case '-':
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            case '.':
-                pos++;
-                break;
-            default:
-                ok = false;
+                case '+':
+                case '-':
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    pos++;
+                    break;
+                default:
+                    ok = false;
             }
         }
-        if (pos == 0)
+        if (pos == 0) {
             return 0f;
-        if (pos == length)
+        }
+        if (pos == length) {
             return Float.parseFloat(string + "f");
+        }
         float f = Float.parseFloat(string.substring(0, pos) + "f");
         string = string.substring(pos);
         // inches
@@ -344,42 +470,45 @@ public class Markup {
     /**
      * New method contributed by: Lubos Strapko
      *
-     * @param string a length in the form of an optional + or - followed by a number and a unit
+     * @param string         a length in the form of an optional + or - followed by a number and a unit
      * @param actualFontSize font-size
      * @return a <code>float</code> number
      * @since 2.1.3
      */
     public static float parseLength(String string, float actualFontSize) {
-        if (string == null)
+        if (string == null) {
             return 0f;
+        }
         int pos = 0;
         int length = string.length();
         boolean ok = true;
         while (ok && pos < length) {
             switch (string.charAt(pos)) {
-            case '+':
-            case '-':
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            case '.':
-                pos++;
-                break;
-            default:
-                ok = false;
+                case '+':
+                case '-':
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    pos++;
+                    break;
+                default:
+                    ok = false;
             }
         }
-        if (pos == 0)
+        if (pos == 0) {
             return 0f;
-        if (pos == length)
+        }
+        if (pos == length) {
             return Float.parseFloat(string + "f");
+        }
         float f = Float.parseFloat(string.substring(0, pos) + "f");
         string = string.substring(pos);
         // inches
@@ -407,6 +536,10 @@ public class Markup {
         if (string.startsWith("ex")) {
             return f * actualFontSize / 2;
         }
+        // percentage of current font size
+        if (string.endsWith("%")) {
+            return (f / 100) * actualFontSize;
+        }
         // default: we assume the length was measured in points
         return f;
     }
@@ -424,19 +557,15 @@ public class Markup {
         String colorName = color.toLowerCase(Locale.ROOT).trim();
         try {
             return WebColors.getRGBColor(colorName);
-        }
-        catch(IllegalArgumentException iae) {
+        } catch (IllegalArgumentException iae) {
             return null;
         }
     }
 
     /**
-     * This method parses a String with attributes and returns a Properties
-     * object.
-     * 
-     * @param string
-     *            a String of this form: 'key1="value1"; key2="value2";...
-     *            keyN="valueN" '
+     * This method parses a String with attributes and returns a Properties object.
+     *
+     * @param string a String of this form: 'key1="value1"; key2="value2";... keyN="valueN" '
      * @return a Properties object
      */
     public static Properties parseAttributes(String string) {
@@ -450,18 +579,22 @@ public class Markup {
         String value;
         while (keyValuePairs.hasMoreTokens()) {
             keyValuePair = new StringTokenizer(keyValuePairs.nextToken(), ":");
-            if (keyValuePair.hasMoreTokens())
+            if (keyValuePair.hasMoreTokens()) {
                 key = keyValuePair.nextToken().trim();
-            else
+            } else {
                 continue;
-            if (keyValuePair.hasMoreTokens())
+            }
+            if (keyValuePair.hasMoreTokens()) {
                 value = keyValuePair.nextToken().trim();
-            else
+            } else {
                 continue;
-            if (value.startsWith("\""))
+            }
+            if (value.startsWith("\"")) {
                 value = value.substring(1);
-            if (value.endsWith("\""))
+            }
+            if (value.endsWith("\"")) {
                 value = value.substring(0, value.length() - 1);
+            }
             result.setProperty(key.toLowerCase(Locale.ROOT), value);
         }
         return result;
@@ -469,17 +602,14 @@ public class Markup {
 
     /**
      * Removes the comments sections of a String.
-     * 
-     * @param string
-     *            the original String
-     * @param startComment
-     *            the String that marks the start of a Comment section
-     * @param endComment
-     *            the String that marks the end of a Comment section.
+     *
+     * @param string       the original String
+     * @param startComment the String that marks the start of a Comment section
+     * @param endComment   the String that marks the end of a Comment section.
      * @return the String stripped of its comment section
      */
     public static String removeComment(String string, String startComment,
-            String endComment) {
+        String endComment) {
         StringBuilder result = new StringBuilder();
         int pos = 0;
         int end = endComment.length();
