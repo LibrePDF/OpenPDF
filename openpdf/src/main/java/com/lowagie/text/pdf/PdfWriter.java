212d082baff7473c638ae9988e2541979d6fe26e
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2024-02-27 10:27:02 +0100

    Provide PDF/A conform annotations

--- openpdf/src/main/java/com/lowagie/text/pdf/PdfWriter.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/PdfWriter.java
@@ -1980,6 +1980,10 @@ public class PdfWriter extends DocWriter implements
         return pdfxConformance.isPdfX();
     }
 
+    public boolean isPdfA1() {
+        return pdfxConformance.isPdfA1();
+    }
+
 //  [C11] Output intents
 
     /**
