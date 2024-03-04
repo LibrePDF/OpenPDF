3507724ccc678e975a141fda3e27a4a9d4d9ac2c
Claudio Clemens <asturio@gmx.net>
2024-03-04 09:58:02 +0100

    PdfDocumentCJKExtensionTest disabled due to missing font

--- openpdf/src/test/java/com/lowagie/text/pdf/PdfDocumentCJKExtensionTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfDocumentCJKExtensionTest.java
@@ -1,65 +1,69 @@
 package com.lowagie.text.pdf;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class PdfDocumentCJKExtensionTest {
-	@Test
-	void generateDocumentsWithCJKExtension() throws IOException {
-		String fontName = "TakaoMjMincho";
 
-		// TakaoMjMincho Version 003.01.01
-		// Please download and place it below. 
-		// https://launchpad.net/takao-fonts
-		// https://launchpad.net/takao-fonts/trunk/15.03/+download/TakaoMjFonts_00301.01.zip
-		String fontPath = "src/test/resources/fonts/TakaoMjFonts/TakaoMjMincho.ttf";
+    // This test is here only for documentation purposes.
+    @Disabled("This test requires a specific font file, which is very large. It is not included in the repository.")
+    @Test
+    void generateDocumentsWithCJKExtension() throws IOException {
+        String fontName = "TakaoMjMincho";
 
-		// register font
-		FontFactory.register(fontPath, fontName);
+        // TakaoMjMincho Version 003.01.01
+        // Please download and place it below.
+        // https://launchpad.net/takao-fonts
+        // https://launchpad.net/takao-fonts/trunk/15.03/+download/TakaoMjFonts_00301.01.zip
+        String fontPath = "src/test/resources/fonts/TakaoMjFonts/TakaoMjMincho.ttf";
 
-		Document document = new Document();
+        // register font
+        FontFactory.register(fontPath, fontName);
 
-		// FOP off 
-		document.setGlyphSubstitutionEnabled(false);
+        Document document = new Document();
 
-		PdfWriter.getInstance(document, new FileOutputStream("target/" + PdfDocumentCJKExtensionTest.class.getSimpleName() + ".pdf"));
+        // FOP off
+        document.setGlyphSubstitutionEnabled(false);
 
-		try {
-			Font font = FontFactory.getFont(fontName, "Identity-H", false, 10, 0, null);
+        PdfWriter.getInstance(document,
+            Files.newOutputStream(
+                Paths.get("target/" + PdfDocumentCJKExtensionTest.class.getSimpleName() + ".pdf")));
 
-			document.open();
-			// CJK Unified Ideographs Extension B
-			// https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_Extension_B
+        try {
+            Font font = FontFactory.getFont(fontName, "Identity-H", false, 10, 0, null);
 
-			// U+20000
-			String cjkB_OK = "𠀀";
-			// U+2A000 
-			String cjkB_NG = "𪀀";
+            document.open();
+            // CJK Unified Ideographs Extension B
+            // https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_Extension_B
 
+            // U+20000
+            String cjkB_OK = "𠀀";
+            // U+2A000
+            String cjkB_NG = "𪀀";
 
-			// CJK Unified Ideographs Extension C
-			// https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_Extension_C
+            // CJK Unified Ideographs Extension C
+            // https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_Extension_C
 
-			// U+2A746
-			String cjkC_NG = "𪝆";
+            // U+2A746
+            String cjkC_NG = "𪝆";
 
+            // CJK Unified Ideographs Extension D
+            // https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_Extension_D
 
-			// CJK Unified Ideographs Extension D
-			// https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_Extension_D
+            // U+2B746
+            String cjkD_NG = "𫝆";
 
-			// U+2B746
-			String cjkD_NG = "𫝆";
+            document.add(new Chunk(cjkB_OK + " " + cjkB_NG + " " + cjkC_NG + " " + cjkD_NG, font));
 
-			document.add(new Chunk(cjkB_OK + " " + cjkB_NG + " " + cjkC_NG + " " + cjkD_NG, font));
-
-		} finally {
-			document.close();
-		}
-	}
+        } finally {
+            document.close();
+        }
+    }
 }
