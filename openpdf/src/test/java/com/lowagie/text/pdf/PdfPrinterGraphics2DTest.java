d83615f8fe6d68d56216948584eb28890a0e031b
Matthias Perktold <7334069+mperktold@users.noreply.github.com>
2024-03-05 11:57:46 +0100

    Replace '*' with explicit import

--- openpdf/src/test/java/com/lowagie/text/pdf/PdfPrinterGraphics2DTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfPrinterGraphics2DTest.java
@@ -1,6 +1,6 @@
 package com.lowagie.text.pdf;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.lowagie.text.Document;
 
