a31c615a3e86b5a0a0956910e53ed7c957a3a7ea
Claudio Clemens <asturio@gmx.net>
2024-03-11 16:57:12 +0100

    Corrected tests to run also on Windows

--- openpdf/src/test/java/com/lowagie/text/pdf/PdfSmartCopyTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfSmartCopyTest.java
@@ -3,7 +3,7 @@ package com.lowagie.text.pdf;
 import com.lowagie.text.Document;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.nio.file.Files;
 import java.time.Duration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,20 +15,19 @@ public class PdfSmartCopyTest {
     }
 
     @Test
-    public void test1() throws IOException {
+    public void test1() {
         File orig = new File("src/test/resources/pdfsmartcopy_bec.pdf");
         check(orig, 1);
     }
 
-
-    private void check(File orig, int count) throws IOException {
-        Assertions.assertTimeout(Duration.ofSeconds(10), () -> {
-            File out = new File("target/test-classes/pdfsmartocy-" + count + ".pdf");
-            out.getParentFile().mkdirs();
+    @SuppressWarnings("SameParameterValue")
+    private void check(File orig, int counter) {
+        Assertions.assertTimeout(Duration.ofSeconds(20), () -> {
+            File out = new File("target/test-classes/pdfsmartocy-" + counter + ".pdf");
+            Files.createDirectories(out.getParentFile().toPath());
             Document document = new Document();
             FileOutputStream outputStream = new FileOutputStream(out);
             PdfCopy copy = new PdfSmartCopy(document, outputStream);
-            //        PdfCopy copy = new PdfCopy(document, outputStream);
             document.open();
 
             PdfReader reader = new PdfReader(orig.getAbsolutePath());
@@ -43,6 +42,5 @@ public class PdfSmartCopyTest {
             copy.close();
         });
     }
-
 
 }
