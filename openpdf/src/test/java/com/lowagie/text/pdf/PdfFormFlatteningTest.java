df3108baeed9b41417f1bf4333f1645287e9c7d3
Alexandr Shandra <alexandr_shandra@docupace.com>
2024-03-23 07:59:16 +0200

    Added support of IndirectObject for coordinates in /Rect, /BBox

--- openpdf/src/test/java/com/lowagie/text/pdf/PdfFormFlatteningTest.java
+++ openpdf/src/test/java/com/lowagie/text/pdf/PdfFormFlatteningTest.java
@@ -5,6 +5,10 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -127,4 +131,38 @@ public class PdfFormFlatteningTest {
             pdfReader.close();
         }
     }
+
+    @Test
+    void testFlattenFieldsWithPdfIndirectObjectInRect() throws IOException {
+        final Path targetFilePath = Paths.get("target/indirect_object_in_rectangle-flattened.pdf");
+        try (InputStream resource = getClass().getResourceAsStream(
+                "/flattening/indirect_object_in_rectangle.pdf")) {
+            Assertions.assertNotNull(resource, "File could not be found!");
+            OutputStream fos = Files.newOutputStream(targetFilePath);
+
+            PdfReader pdfReader = new PdfReader(resource);
+            PdfStamper stamper = new PdfStamper(pdfReader, fos);
+
+            stamper.getAcroFields().setGenerateAppearances(true);
+            stamper.setFormFlattening(true);
+
+            pdfReader.close();
+            stamper.close();
+        }
+        //Verify no form fields left (the correct shape is difficult to verify...)
+        try (InputStream resource = Files.newInputStream(targetFilePath)) {
+
+            Assertions.assertNotNull(resource, "File could not be found!");
+            PdfReader pdfReader = new PdfReader(resource);
+
+            PdfDictionary acroForm = (PdfDictionary) PdfReader.getPdfObjectRelease(
+                    pdfReader.getCatalog().get(PdfName.ACROFORM));
+            Assertions.assertTrue(acroForm == null
+                    || acroForm.getAsArray(PdfName.FIELDS) == null
+                    || acroForm.getAsArray(PdfName.FIELDS).isEmpty());
+
+            pdfReader.close();
+        }
+    }
+
 }
