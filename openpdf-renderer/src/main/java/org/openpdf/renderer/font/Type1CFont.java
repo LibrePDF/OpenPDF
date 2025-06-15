fae110015fd0cd8a86b83974a2739cbdbe19ae19
Andreas Røsdal <andreas.rosdal@gmail.com>
2025-06-15 20:37:31 +0200

    Fix warning.

--- openpdf-renderer/src/main/java/org/openpdf/renderer/font/Type1CFont.java
+++ openpdf-renderer/src/main/java/org/openpdf/renderer/font/Type1CFont.java
@@ -210,7 +210,10 @@ public class Type1CFont extends OutlineFont {
                 break;
             }
         }
-        this.fnum = (float) (((neg ? -1.0 : 1.0) * (double) f) * Math.pow(10.0, eval * exp));
+        double sign = neg ? -1.0 : 1.0;
+        double base = sign * f;
+        double result = base * Math.pow(10.0, eval * exp);
+        this.fnum = (float) result;
     }
 
     /**
