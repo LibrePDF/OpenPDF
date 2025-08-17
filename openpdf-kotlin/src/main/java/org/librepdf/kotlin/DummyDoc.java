0423baebc44f0da5454958a3613cc102384e22a5
Claudio Clemens <asturio@gmx.net>
2025-08-17 13:05:48 +0200

    fix: DommyDoc for kotlin is back

---
+++ openpdf-kotlin/src/main/java/org/librepdf/kotlin/DummyDoc.java
@@ -0,0 +1,11 @@
+package org.librepdf.kotlin;
+
+/**
+ * DummyDoc is a placeholder to generate a valid javadoc.jar
+ *
+ * WARNING: Only delete this file if there is an alternative documentation file beeing generated. Maven Central needs
+ * a doc file, or else we can't release new versions.
+ */
+public class DummyDoc {
+    // Intentionally empty
+}
