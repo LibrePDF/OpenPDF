896944a4e565f6a9ffa38fc8a2d5e078dc944ef8
Sławomir Tomaszek <s.tomaszek@post.pl>
2026-04-22 22:00:44 +0200

    checkstyle - formatting - part 11

--- openpdf-html/src/main/java/org/openpdf/css/parser/FSColor.java
+++ openpdf-html/src/main/java/org/openpdf/css/parser/FSColor.java
@@ -24,6 +24,7 @@ import com.google.errorprone.annotations.CheckReturnValue;
 public interface FSColor {
     @CheckReturnValue
     FSColor lightenColor();
+
     @CheckReturnValue
     FSColor darkenColor();
 }
